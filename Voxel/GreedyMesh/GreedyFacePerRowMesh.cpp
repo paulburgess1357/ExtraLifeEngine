078377477e938a0dce5a42b78989cce8a1115dea
Paul B <paulburgess1357@gmail.com>
2021-02-16 19:21:39 -0700

    merge is fine as is

--- Voxel/GreedyMesh/GreedyFacePerRowMesh.cpp
+++ Voxel/GreedyMesh/GreedyFacePerRowMesh.cpp
@@ -2,15 +2,6 @@
 
 std::vector<VertexAndNormals> GreedyFacePerRowMesh::merge_all_faces(const std::vector<VertexAndNormals>& vertex, const FaceType face_type) {
 
-	//TODO i can adjust this.  I don't have to merge each time.  I think I can
-	//TODO continue iterating across the faces until I can't include a block.
-	//TODO when I reach that point, I do the merge.  This would save some time
-	//TODO as I can do one merge only when its more than 2 blocks.  For example.
-	//TODO if it was 6 blocks, that would be five merges.  However, if I do it
-	//TODO more efficient, I can do that in one merge.  I just have to make sure
-	//TODO blocks are the same type and sequential in between
-
-
 	// Six vertices per face;  If the total number of vertices is <= 6 there is
 	// no need to attempt the merge as we only have one face
 	if (vertex.size() <= 6) {
@@ -32,7 +23,7 @@ std::vector<VertexAndNormals> GreedyFacePerRowMesh::merge_all_faces(const std::v
 		// Merge faces of possible.  Regardless if its possible or not,
 		// the last face in the merged_row_vector becomes our new start face
 		if (is_mergeable(start_face, next_face, face_type)) {
-			merge_face(merged_row_vector, next_face, face_type, i);
+			merge_face(merged_row_vector, next_face, face_type);
 		} else {
 			pushback_face(merged_row_vector, next_face);
 		}
@@ -48,7 +39,7 @@ bool GreedyFacePerRowMesh::is_mergeable(const Face& start_face, const Face& next
 		depths_match(start_face, next_face, face_type) && is_adjacent(start_face, next_face, face_type);
 }
 
-void GreedyFacePerRowMesh::merge_face(std::vector<VertexAndNormals>& merged_vector, const Face& next_face, const FaceType face_type, const size_t start_idx) {
+void GreedyFacePerRowMesh::merge_face(std::vector<VertexAndNormals>& merged_vector, const Face& next_face, const FaceType face_type) {
 
 	// Merge (note that we only need to merge the 'next' face
 	// into our 'start face' slot.  Rather than putting all 6 new

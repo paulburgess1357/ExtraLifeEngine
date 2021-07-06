2f3a9a32cb44592b20426a86f739b8ad59075a5f
Paul B <paulburgess1357@gmail.com>
2021-07-06 15:07:14 -0600

    shader compiler partially updated

--- Environment/Interfaces/Shader/IShaderCompiler.cpp
+++ Environment/Interfaces/Shader/IShaderCompiler.cpp
@@ -3,20 +3,18 @@
 #include "../../Utility/FatalError.h"
 #include "../../Environment/OpenGL/Shader/OpenGLShaderCompiler.h"
 
-std::shared_ptr<IShaderCompiler> IShaderCompiler::create_compiler(const std::shared_ptr<IShaderLoader>& shader_loader){
+std::unique_ptr<IShaderCompiler> IShaderCompiler::create_compiler(const std::shared_ptr<IShaderLoader>& shader_loader){
 
+	std::unique_ptr<IShaderCompiler> shader_compiler = nullptr;
+	
 	if (GraphicsAPI::get_api() == GraphicsAPIType::OPENGL) {
-		return std::make_shared<OpenGL::OpenGLShaderCompiler>(shader_loader);
-	}
-
-	if (GraphicsAPI::get_api() == GraphicsAPIType::VULKAN) {
-		std::shared_ptr<IShaderCompiler> shader_compiler = nullptr;
+		shader_compiler = std::make_unique<OpenGL::OpenGLShaderCompiler>(shader_loader);
+	} else if (GraphicsAPI::get_api() == GraphicsAPIType::VULKAN) {
 		FatalError::fatal_error("Vulkan shader compiler does not exist!.");
-		return shader_compiler;
+	} else{
+		FatalError::fatal_error("Unknown graphics API type.  Cannot return shader compiler.");
 	}
-
-	FatalError::fatal_error("Unknown graphics API type.  Cannot return shader compiler.");
-	std::shared_ptr<IShaderCompiler> shader_compiler = nullptr;
+	
 	return shader_compiler;
 	
 }
--- Environment/Interfaces/Shader/IShaderCompiler.h
+++ Environment/Interfaces/Shader/IShaderCompiler.h
@@ -13,7 +13,7 @@ public:
 	
 	virtual ~IShaderCompiler() = default;
 	[[nodiscard]] virtual std::unique_ptr<IShaderProgram> compile() const = 0;
-	[[nodiscard]] static std::shared_ptr<IShaderCompiler> create_compiler(const std::shared_ptr<IShaderLoader>& shader_loader);
+	[[nodiscard]] static std::unique_ptr<IShaderCompiler> create_compiler(const std::shared_ptr<IShaderLoader>& shader_loader);
 	
 protected:
 	std::pair<std::string, std::string> m_vertex_fragment_strings;

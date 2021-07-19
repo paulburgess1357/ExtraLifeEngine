03756f1caeba41ae01e6b47a3b6cbed408044d1b
Paul B <paulburgess1357@gmail.com>
2021-07-19 08:48:25 -0600

    phong in vertex shader written

--- Assets/shaders/model_shaders_view_space/colored_cube/vertex_diffuse_shading.glsl
+++ Assets/shaders/model_shaders_view_space/colored_cube/vertex_diffuse_shading.glsl
@@ -31,14 +31,14 @@ void main(){
     vec3 light_position_eye = vec3(view_matrix * vec4(light_position_in_world, 1.0));
 
     // Convert cube normals and cube position to view space (eye/camera space)
-    // Note that the model matrix has already been multiplied by the view matrix by the time its sent to the shader
+    // Note that the model matrix has already been multiplied by the view matrix by the time its sent to the shader (for normal matrix calculation)
     vec3 cube_normals_eye = normalize(vec3(normal_matrix * cube_normals));
     vec3 cube_position_eye = vec3(view_matrix * model_matrix * vec4(cube_position, 1.0));
     vec3 light_direction_from_surface_to_lightsource = normalize(light_position_eye - cube_position_eye);
 
     // Diffuse shading equation
     // Note that ins the max(dot()) formula, both variables are unit vectors (normalized)
-    // Therefore, the lighting is impacted by the angle (so vector magnitude as no impact on dot product)
+    // Therefore, the lighting is impacted by the angle (so vector magnitude has no impact on dot product)
     // I.e. the dot product of 2 unit vectors equals the cos of the angle.
     calculated_frag_color = diffuse_color * light_intensity * max(dot(light_direction_from_surface_to_lightsource, cube_normals_eye), 0.0);
 
